4a4a6f330295d1d3e2a8efa7d7b2b42aeb64c130
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-01-11 01:22:48 -0500

    another way to add template inside app

--- base/views.py
+++ base/views.py
@@ -6,8 +6,17 @@ from django.http import HttpResponse
 
 # how to present the view, rendering, handle data presentation
 
+
+rooms = [
+    {'id': 1, 'name': 'lets cycling'},
+    {'id': 2, 'name': 'Gear Shoper'},
+    {'id': 3, 'name': 'Pro rider'},
+]
+
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
+
 
 def room(request):
     return render(request, 'room.html')
