d1e9835d14a716a976c793d03b33ece0f83e6f1f
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-02-05 22:17:09 -0500

    User Login - log out

--- base/urls.py
+++ base/urls.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 urlpatterns = [
     path('login/', views.loginPage, name = 'login'),
-
+    
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
 
