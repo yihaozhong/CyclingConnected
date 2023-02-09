1e573ed890292187c3e252bf51f27ba270b7bd4a
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-02-08 23:23:28 -0500

    Chat room - conversation and comment input

--- base/views.py
+++ base/views.py
@@ -101,8 +101,8 @@ def room(request, pk):
         # use get() from the datatable
     room = Room.objects.get(id=pk)
 
-    messages = room.message_set.all()
-    context = {'room': room, 'messages':messages}
+    room_messages = room.message_set.all().order_by('-created')
+    context = {'room': room, 'room_messages':room_messages}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url = '/login')
