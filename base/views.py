b5d10b34b7205d7970453bdfb7ab5bd12c5290db
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-02-11 16:29:25 -0500

    Chatroom - delete msg

--- base/views.py
+++ base/views.py
@@ -158,3 +158,16 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+
+@login_required(login_url = '/login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id = pk)
+
+    if request.user != message.user:
+        return  HttpResponse('Your are not allowed here!!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
