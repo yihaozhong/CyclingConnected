4feca4d0c8bb7c4310d259bc2ba86ccd9666d45d
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-02-18 23:52:11 -0500

    Edit profile - add new edit profile

--- base/views.py
+++ base/views.py
@@ -197,3 +197,8 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': message})
+
+@login_required(login_url = '/login')
+def updateUser(request):
+    context = {}
+    return render(request, 'base/update-user.html', context)
