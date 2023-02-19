ef446b23eb1898b64e5efee6d4f538d9fa16e6f7
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-02-19 17:22:38 -0500

    test the api

--- cyclingconnected/settings.py
+++ cyclingconnected/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
 
     'rest_framework', 
+
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +54,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "cyclingconnected.urls"
@@ -128,3 +132,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
